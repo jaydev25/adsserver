a91ab22932a7ea5e41775ea9ed1f726334a158c1
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-09-08 02:01:45 -0700

    Joi Validation added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,46 +14,64 @@ if (process.env.NODE_ENV === 'development') {
 const crypto = require('crypto-random-string');
 const db = require('../storage/main/models');
 const sendVerificationEmail = require('../api/sendverificationmail/controller');
-
+const Joi = require('joi');
 // The authentication controller.
 var AuthController = {};
 
 // Register a user.
 AuthController.signUp = function(req, res) {
-    const newUser = {
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        createdBy: req.body.createdBy,
-        updatedBy: req.body.updatedBy,
-    };
-    if (process.env.NODE_ENV === 'development') {
-        newUser.password = bcrypt.hashSync(req.body.password);
-    } else {
-        newUser.password = bcrypt.hashSync(req.body.password, salt);
-    }
-    // Attempt to save the user
-    return db.Users.findOrCreate({
-        where: { email:  req.body.email },
-        defaults: newUser
-    }).spread((user, created) => {
-        // if user email already exists
-        if(!created) {
-            return res.status(409).json('User with email address already exists');
+    const schema = Joi.object().keys({
+        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required()
+    }).options({
+        stripUnknown: true
+    });
+
+    return Joi.validate(req.body, schema, function (err, value) {
+        if (err) {
+            return res.status(422).json({
+                status: err.name,
+                data: err.details
+            });
         } else {
-            return db.VerificationToken.create({
-                userId: user.id,
-                token: crypto(16)
-            }).then((result) => {
-                sendVerificationEmail(req.body.email, result.token);
-                return res.status(200).json(`${req.body.email} account created successfully`);
+            const newUser = {
+                email: req.body.email,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                createdBy: req.body.email,
+                updatedBy: req.body.email,
+            };
+            if (process.env.NODE_ENV === 'development') {
+                newUser.password = bcrypt.hashSync(req.body.password);
+            } else {
+                newUser.password = bcrypt.hashSync(req.body.password, salt);
+            }
+            // Attempt to save the user
+            return db.Users.findOrCreate({
+                where: { email:  req.body.email },
+                defaults: newUser
+            }).spread((user, created) => {
+                // if user email already exists
+                if(!created) {
+                    return res.status(409).json('User with email address already exists');
+                } else {
+                    return db.VerificationToken.create({
+                        userId: user.id,
+                        token: crypto(16)
+                    }).then((result) => {
+                        sendVerificationEmail(req.body.email, result.token);
+                        return res.status(200).json(`${req.body.email} account created successfully`);
+                    }).catch((error) => {
+                        return res.status(500).json(error);
+                    });
+                }
             }).catch((error) => {
                 return res.status(500).json(error);
             });
         }
-    }).catch((error) => {
-        return res.status(500).json(error);
-    });
+    });  // err === null -> valid
 }
 
 // Compares two passwords.
@@ -68,38 +86,50 @@ function comparePasswords(password, userPassword, callback) {
 
 // Authenticate a user.
 AuthController.authenticateUser = function(req, res) {
-    if(!req.body.email || !req.body.password) {
-        res.status(404).json({ message: 'email and password are needed!' });
-    } else {
-        const email = req.body.email,
-            password = req.body.password,
-            potentialUser = { where: { email: email } };
+    const schema = Joi.object().keys({
+        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
+    }).options({
+        stripUnknown: true
+    });
 
-        db.Users.findOne(potentialUser).then(function(user) {
-            if(!user) {
-                res.status(404).json({ message: 'Authentication failed!' });
-            } else {
-                comparePasswords(password, user.password, function(error, isMatch) {
-                    if(isMatch && !error) {
-                        var token = jwt.sign(
-                            { email: user.email },
-                            config.keys.secret
-                        );
+    return Joi.validate(req.body, schema, function (err, value) {
+        if (err) {
+            return res.status(422).json({
+                status: err.name,
+                data: err.details
+            });
+        } else {
+            const email = req.body.email,
+                password = req.body.password,
+                potentialUser = { where: { email: email } };
 
-                        res.json({
-                            success: true,
-                            token: 'JWT ' + token,
-                            role: user.role
-                        });
-                    } else {
-                        res.status(404).json({ message: 'Login failed!' });
-                    }
-                });
-            }
-        }).catch(function(error) {
-            res.status(500).json({ message: 'There was an error!' });
-        });
-    }
+            db.Users.findOne(potentialUser).then(function(user) {
+                if(!user) {
+                    res.status(404).json({ message: 'Authentication failed!' });
+                } else {
+                    comparePasswords(password, user.password, function(error, isMatch) {
+                        if(isMatch && !error) {
+                            var token = jwt.sign(
+                                { email: user.email },
+                                config.keys.secret
+                            );
+
+                            res.json({
+                                success: true,
+                                token: 'JWT ' + token,
+                                role: user.role
+                            });
+                        } else {
+                            res.status(404).json({ message: 'Login failed!' });
+                        }
+                    });
+                }
+            }).catch(function(error) {
+                res.status(500).json({ message: 'There was an error!' });
+            });
+        }
+    });
 }
 
 module.exports = AuthController;
